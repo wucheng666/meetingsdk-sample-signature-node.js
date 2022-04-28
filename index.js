e936c602a76f428ee48515af112c735522c8783b
吴成 <wubaodtc@sina.com>
2022-04-28 21:57:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,40 @@ app.post('/demo', (req, res) => {
   })
 })
 
+app.post("/getAccessToken0", (req, res) => {
+  cors(req, res, () => {
+    // Request an access token using the auth code
+    let url = `https://zoom.us/oauth/token?grant_type=authorization_code&code=${req.body.code}&redirect_uri=${encodeURIComponent(process.env.redirectURL)}`;
+  
+    request
+      .post(url, (error, response, body) => {
+        console.log("error", error);
+        console.log("body", body);
+
+        // Parse response to JSON
+        body = JSON.parse(body);
+
+        // Logs your access and refresh tokens in the browser
+        console.log(`access_token: ${body.access_token}`);
+        console.log(`refresh_token: ${body.refresh_token}`);
+
+        res.json({
+          response: body,
+        });
+        if (body.access_token) {
+          // Step 4:
+          // We can now use the access token to authenticate API calls
+          // Send a request to get your user information using the /me context
+          // The `/me` context restricts an API call to the user the token belongs to
+          // This helps make calls to user-specific endpoints instead of storing the userID
+        } else {
+          // Handle errors, something's gone wrong!
+        }
+      })
+      .auth(process.env.ZOOM_SDK_KEY, process.env.ZOOM_SDK_SECRET);
+    })
+});
+
 app.post("/getAccessToken", (req, res) => {
     // Request an access token using the auth code
     let url = `https://zoom.us/oauth/token?grant_type=authorization_code&code=${req.body.code}&redirect_uri=${encodeURIComponent(process.env.redirectURL)}`;
